c1d3dbf53da7f613313d5efce36923b6f738aeba
Larissa <larissaralmeida.94@gmail.com>
2025-03-23 18:02:40 -0300

    Create login test

---
+++ cypress/e2e/signIn.cy.js
@@ -0,0 +1,26 @@
+import signInPage from "../support/pages/signInPage";
+
+describe("Login de usuário", () => {
+
+    let userData;
+
+    beforeEach(function () {
+        cy.fixture('user').then((data) => {
+            userData = data;
+        });
+    });
+
+    it('Realizar login com sucesso utilizando credenciais válidas', () => {
+
+        //Garante que os dados estejam carregados antes de continuar o teste
+        cy.wrap(userData).should('exist');
+
+        signInPage.visit();
+        signInPage.fillEmail(userData.email);
+        signInPage.fillPassword(userData.password);
+        signInPage.submit();
+
+
+    });
+
+});
